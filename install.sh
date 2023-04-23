2abf0a8e2fc1e7ab7f0f85f0388a6591adb64c4b
Dax T. Games <76230985+296951@users.noreply.github.com>
2023-04-23 15:21:47 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing daxgames's YADR for the first time"
     git clone --depth=1 https://github.com/daxgames/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
+    git checkout linux
     [ "$1" = "ask" ] && export ASK="true"
     rake install
 else
