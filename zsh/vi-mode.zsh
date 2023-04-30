2bb0b65839294ce767e5123879a5dfb003aad824
Luiz Filho <lfilho@gmail.com>
2023-04-29 22:51:41 -0700

    feat: better vimification of the terminal

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,4 +1,6 @@
 set -o vi
 export EDITOR=nvim
 export VISUAL=nvim
-export MANPAGER="nvim -c 'set ft=man' -"
+export MANPAGER="nvim +Man!"
+
+source $(brew --prefix)/opt/zsh-vi-mode/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
