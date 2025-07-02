39755c124cde4acf501091caa6121543028af987
Luiz Filho <lfilho@gmail.com>
2025-07-01 21:07:39 -0700

    feat: replace p10k with omp

--- zsh/zzzz_after.zsh
+++ zsh/zzzz_after.zsh
@@ -4,3 +4,8 @@ if [ -d $HOME/.zsh.after/ ]; then
     for config_file ($HOME/.zsh.after/*.zsh) source $config_file
   fi
 fi
+
+if [ "$TERM_PROGRAM" != "Apple_Terminal" ]; then
+  # eval "$(oh-my-posh init zsh)"
+  eval "$(oh-my-posh init zsh --config ~/.yadr/zsh/oh-my-posh-theme.json)"
+fi
