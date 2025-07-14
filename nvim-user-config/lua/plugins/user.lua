f4ee9b5d6bd85be2858e43f0d6daaf08553a8cc0
Luiz Gonzaga <lfilho@gmail.com>
2025-07-14 08:39:02 -0700

    style: more comestic changes

--- nvim-user-config/lua/plugins/user.lua
+++ nvim-user-config/lua/plugins/user.lua
@@ -13,7 +13,6 @@ return {
       })
     end,
   },
-
   {
     "MeanderingProgrammer/render-markdown.nvim",
     -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
