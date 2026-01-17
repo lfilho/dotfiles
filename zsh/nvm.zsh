6e2010034810d06345bb214885c978d236bf84f4
Luiz Gonzaga <lgonzaga@coursera.org>
2026-01-17 00:54:28 -0800

    perf: lazy load nvm to speed zsh up

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,3 +1,32 @@
+# Lazy-load nvm - only initialize when actually used
 export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+  # Create wrapper functions for common node commands
+  function nvm() {
+    unfunction nvm node npm npx 2>/dev/null
+    source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+    nvm "$@"
+  }
+
+  function node() {
+    unfunction nvm node npm npx 2>/dev/null
+    source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+    node "$@"
+  }
+
+  function npm() {
+    unfunction nvm node npm npx 2>/dev/null
+    source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+    npm "$@"
+  }
+
+  function npx() {
+    unfunction nvm node npm npx 2>/dev/null
+    source "$NVM_DIR/nvm.sh"
+    [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+    npx "$@"
+  }
+fi
